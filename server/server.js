f682fe3706edc54bef33e9a3d35d64867b20536d
Swaeba-Bilal <swaibabilal02@gmail.com>
2026-08-18 18:43:06 +0500

    Added the vercel.json

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const mongoose = require("mongoose");
 const app = require("./app");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI.replace("<DB_PASSWORD>", 
-    process.env.DATABASE_PASSWORD))
-    .then(()=>console.log("MongoDB COnnected")
-    ).catch((error) =>{
-        console.log("MongoDb Connection error", error);
-        
-    })
-const port = process.env.PORT || 5001;
-app.listen(port, () =>{
-    console.log(`Server running on port ${port}`);
-    
-});
+mongoose
+  .connect(
+    process.env.MONGODB_URI.replace(
+      "<DB_PASSWORD>",
+      process.env.DATABASE_PASSWORD
+    )
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => {
+    console.log("MongoDB Connection error", error);
+  });
+
+module.exports = app;
