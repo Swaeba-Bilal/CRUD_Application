a58ef01ee5101e49098ae803f3d2e68082d9e3c1
Swaeba-Bilal <swaibabilal02@gmail.com>
2026-08-19 00:00:02 +0500

    Fix CORS for deployed frontend

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const cors = require("cors");
 const app = express();
 const userRouter = require("./routes/userRoutes");
@@ -9,4 +9,23 @@ app.use(express.urlencoded({extended: true}));
 
 //Routes
 app.use("/api/v2/users", userRouter)
+module.exports = app;*/
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const userRouter = require("./routes/userRoutes");
+
+app.use(
+  cors({
+    origin: "https://crud-application-sni3-umber.vercel.app",
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v2/users", userRouter);
+
 module.exports = app;
